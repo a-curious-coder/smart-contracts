361d5a903cce85af9701604f5d4f8557820af379
zardoss <callummclennan1@googlemail.com>
2021-06-19 23:00:32 +0100

    Initial Commit

---
+++ greeter/test/greeter_test.js
@@ -0,0 +1,21 @@
+const { assert } = require("console");
+
+// Loads contract via "artifacts.require" function
+const GreeterContract = artifacts.require("Greeter");
+
+contract("Greeter", () => {
+    it("has been deployed successfully", async () => {
+        const greeter = await GreeterContract.deployed();
+        assert(greeter, "contract has been deployed");
+    });
+});
+
+describe("greet()", () => {
+    it("returns 'Hello, World!'", async () => {
+        const greeter  = await GreeterContract.deployed();
+        const expected = "Hello, World!";
+        const actual = await greeter.greet();
+        
+        assert.equal(actual, expected, "greeted with 'Hello, World!'");
+    });
+});
